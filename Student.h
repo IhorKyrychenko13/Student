656d5823ed3458903fa0f86effad65f092b665d3
Ihor Kyrychenko <legouflandia@gmail.com>
2025-07-01 18:37:15 +0200

    Добавьте файлы проекта.

---
+++ Student.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Student {
+private:
+    char* fullName;
+    double averageGrade;
+    char university[50];
+    char groupNumber[10];
+
+public:
+    Student(); // Конструктор за замовчуванням
+    Student(const char* name, double grade, const char* univ, const char* group);
+    ~Student(); // Деструктор
+
+    void showInfo();
+
+    void setName(const char* name);
+    void setGrade(double grade);
+
+    const char* getName();
+    double getGrade();
+    const char* getUniversity();
+    const char* getGroup();
+};
